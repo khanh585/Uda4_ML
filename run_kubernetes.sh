cbaa4b771bfa33e4fbf5d7167397ef8600dbfa37
khanh-hoho <khanhtd@ti-one.de>
2022-12-17 17:25:34 +0700

    1

---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath="khanh585/microproject:v1.0.1"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run microproject \
+    --image=khanh585/microproject:v1.0.1 \
+    --port=80 --labels app=microproject
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods 
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward microproject 8000:80
+
